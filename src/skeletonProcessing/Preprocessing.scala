d729d1150feffe17fa563f571bba758bd8951a4a
Leezl <wolfdrake15@hotmail.com>
2012-04-21 16:26:01 -0700

    Tried to get skeletons in, then couldn't find their format. Turns out it IS standard.

---
+++ src/skeletonProcessing/Preprocessing.scala
@@ -0,0 +1,22 @@
+package skeletonProcessing
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Liesl Wigand
+ * Date: 4/21/12
+ * Time: 2:40 PM
+ * To change this template use File | Settings | File Templates.
+ *
+ * This file contains the methods for altering the skeltons from
+ * bvh and acclaim format into a standard form.
+ */
+
+class Preprocessing {
+  var newrep : List[Double]
+  var grdvel : List[Double]
+  var velocity : Double
+
+  def preprocessing1(skel : Skeleton){
+
+  }
+}
