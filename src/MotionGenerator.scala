6ea354544db2ec3563bfd6edf60670604ef13386
Leezl <wolfdrake15@gmail.com>
2012-04-22 10:23:59 -0700

    Organized Skeleton description; added random variables sorted Preprocessing.

--- src/MotionGenerator.scala
+++ src/MotionGenerator.scala
@@ -1,3 +1,5 @@
+import util.Random
+
 /**
  * Created by IntelliJ IDEA.
  * User: lieslw
@@ -9,5 +11,18 @@
  */
 
 class MotionGenerator {
+  //set initial variables
+  //nX = Model order: time looked forward and back in each layer
+  val n1 = 3
+  val n2 = 3
+  //Randoms: need uniform and gaussian random weights for matrices in crbms;
+  //  may need 2 separate? can't see where they needed uniform dist.
+  //  rand.nextDouble, rand.nextGaussian
+  val rand = new Random(System.currentTimeMillis())
 
+  //load data (file/list of files)
+
+  //Downsample/ preprocess (Put these together? why didn't they?)
+
+  //
 }
--- src/skeletonProcessing/Skeleton.scala
+++ src/skeletonProcessing/Skeleton.scala
@@ -9,29 +9,35 @@ package skeletonProcessing
  *
  * Clearly some trouble committing from Windows. This file is for
  * storing the skeleton structure. I believe we only need one type. Form is:
- * elv<aX> pelv<aY> pelv<aZ> pelv<tX> pelv<tY> pelv<tZ> lfem<aX> ...
+ * pelv<aX> pelv<aY> pelv<aZ> pelv<tX> pelv<tY> pelv<tZ> lfem<aX> ...
  * The rest of the numbers in the line specify the remaining joints.
  * These joints are specified in the following order:
- * pelvis
-    lfemur
-      ltibia
-        lfoot
-          ltoes
-    rfemur
-      rtibia
-        rfoot
-          rtoes
-    thorax
-      lclavicle
-        lhumerus
-          lradius
-            lhand
-      rclavicle
-        rhumerus
-          rradius
-            rhand
+   pelvis 1->2 6 10
+    lfemur 2->3
+      ltibia 3->4
+        lfoot 4->5
+          ltoes 5->
+    rfemur 6->7
+      rtibia 7->8
+        rfoot 8->9
+          rtoes 9->
+    thorax 10-> 11 15
+      lclavicle 11->12
+        lhumerus 12->13
+          lradius 13->14
+            lhand 14->
+      rclavicle 15->16
+        rhumerus 16->17
+          rradius 17->18
+            rhand 18->
  */
 
 class Skeleton {
+  //set storage form: tree, or whatever
+
+  //def load function: check file type: bvh, asf & acm, or txt?
+  def loadSkeleton(filename : String) {
+
+  }
 
 }
